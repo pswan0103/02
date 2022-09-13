e46648733fc1befaf6d00dd4b2d371992377ae2a
pswan0103 <22zch@naver.com>
2022-09-13 16:58:05 +0900

    sec

--- main.c
+++ main.c
@@ -5,5 +5,10 @@
 
 int main(int argc, char *argv[]) {
 	printf("Hello, Sookmyung Electronics!\n");
+	printf(10 + 20 = 30);
+	int a, b;
+	a = 10;
+	b = 20;
+	printf("%d + %d = %d\n", a, b, a+b);
 	return 0;
 }
